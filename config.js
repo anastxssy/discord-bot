a36724ad99c9fc3a19036e25b0cad15bb9c1e96f
Anastassy <149289748+anastxssy@users.noreply.github.com>
2023-12-25 22:57:18 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    bot: {
+        appId: '',
+        token: '',
+    },
+    mode: {
+        name: '',
+    },
+    mongodb: {
+        url: '',
+        dbName: '',
+    },
+    owners: []
+}
